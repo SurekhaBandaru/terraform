4f772ce706865c9f2cf91ff2f8f0231973041684
Surekha <surekhagidithuri@gmail.com>
2025-06-10 06:29:55 +0530

    tf vars

---
+++ variables/ec2.tf
@@ -0,0 +1,32 @@
+
+resource "aws_instance" "roboshop" {
+    ami             = var.ami_id #ami_id in variable.tf
+    instance_type   = var.instance_type
+    vpc_security_group_ids = [ aws_security_group.allow_all.id ]
+    tags = var.ec2_tags
+
+}
+
+resource "aws_security_group" "allow_all" {
+    name        =  var.sg_name
+    description =  var.sg_description
+
+    ingress {
+        from_port=var.from_port
+        to_port=var.to_port
+        protocol=var.protocol
+        cidr_blocks=var.cidr_blocks
+        ipv6_cidr_blocks=var.ipv6_cidr_blocks
+
+    }
+    egress {
+        from_port=var.from_port
+        to_port=var.to_port
+        protocol=var.protocol
+        cidr_blocks=var.cidr_blocks
+        ipv6_cidr_blocks=var.ipv6_cidr_blocks
+
+    }
+
+    tags= var.sg_tags
+}
